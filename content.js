ae0884eca4377f6ca8c41cd978bb57cd1d8e8932
hakimuddinhh <hakimuddinhh@gmail.com>
2021-04-25 17:55:49 +0530

    removed unecessary config

--- content.js
+++
@@ -1,6 +0,0 @@
-// content.js
-
-chrome.runtime.sendMessage({type: 'onload'}, (response) => {
-    // do stuff with response (which will be the value of messageQueue
-    // sent from background.js).
-});
